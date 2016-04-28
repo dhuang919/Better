e8f4f693b94a006e01c4f47456ef38ed5a5ae899
Derek Huang <derekh919@gmail.com>
2016-04-28 18:38:49 -0400

    (style) Refactors api.js into ES6

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,8 +1,8 @@
 module.exports = {
-  handleErrors: function (response) {
-      if (!response.ok) {
-          throw Error(response.statusText);
-      }
-      return response;
+  handleErrors (response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
   },
 }

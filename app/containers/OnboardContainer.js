8663144b309431eb801f8e947ae1a3346fe64637
Derek Huang <derekh919@gmail.com>
2016-04-28 21:39:14 -0400

    (refactor) Converts OnboardContainer.js into ES6

--- app/containers/OnboardContainer.js
+++ app/containers/OnboardContainer.js
@@ -1,28 +1,20 @@
-var Swiper = require('react-native-swiper');
+import React, {
+  View,
+  Navigator,
+} from 'react-native';
+import {
+  Welcome,
+  PageOne,
+  PageTwo
+} from '../components/Onboard';
+import Swiper from 'react-native-swiper';
 
-var React = require('react-native');
-var View = React.View;
-var Navigator = React.Navigator;
-var OnboardPageOne = require('../components/OnboardPageOne');
-var OnboardPageTwo = require('../components/OnboardPageTwo');
-var Welcome = require('../components/Welcome');
-
-var OnboardContainer = React.createClass({
-  render: function () {
-    return (
-      <View style={{ flex: 1 }}>
-        <Navigator
-          renderScene={this.renderScene}
-          navigator={this.props.navigator} />
-      </View>
-    );
-  },
-
-  renderScene: function (route, navigator) {
+const OnboardContainer = React.createClass({
+  render () {
     return (
       <Swiper loop={false}>
-        <OnboardPageOne />
-        <OnboardPageTwo />
+        <PageOne />
+        <PageTwo />
         <Welcome navigator={this.props.navigator} />
       </Swiper>
     );

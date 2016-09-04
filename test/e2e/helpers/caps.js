98870b7a9313c38301e765bd009165661d45559a
Derek Huang <derekh919@gmail.com>
2016-09-04 15:32:18 -0400

    Starts e2e setup

---
+++ test/e2e/helpers/caps.js
@@ -0,0 +1,8 @@
+exports.ios93 = {
+  browserName: '',
+  'appium-version': '1.3',
+  platformName: 'iOS',
+  platformVersion: '9.3',
+  deviceName: 'iPhone 5s',
+  app: undefined // will be set later
+};

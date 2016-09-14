f1148c7ceb775d03a0bcff82d7d3d8d0206c8281
Derek Huang <derekh919@gmail.com>
2016-09-14 15:03:45 -0400

    Begins indexios.spec

---
+++ test/unit/indexios.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+// React Native components
+import React, {
+  View,
+  Text,
+  Image,
+  TabBarIOS,
+} from 'react-native';
+// Tab container
+import TabContainer from '../../index.ios';
+// Custom components
+
+// Testing dependencies
+
+// Wrappers
+const tabWrapper = shallow(<TabContainer />);
+
+describe('Tab Container (index.ios)', () => {
+  it('should ', () => {
+
+  });
+
+  describe('Methods', () => {
+
+  });
+});

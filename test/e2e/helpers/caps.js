929a86d9240e789c1e8ae8662b3331ab1167e852
Derek Huang <derekh919@gmail.com>
2016-10-04 16:27:23 -0400

    Adds path for Better.app

--- test/e2e/helpers/caps.js
+++ test/e2e/helpers/caps.js
@@ -1,8 +1,8 @@
 exports.ios93 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.5.3',
   platformName: 'iOS',
   platformVersion: '9.3',
-  deviceName: 'iPhone 5s',
-  app: undefined // will be set later
+  deviceName: 'iPhone 6',
+  app: "/Users/derekhuang/Library/Developer/Xcode/DerivedData/thesis-bpeyrcglajbpqdfqhqewzoxheecu/Build/Products/Debug-iphonesimulator/Better.app"
 };
